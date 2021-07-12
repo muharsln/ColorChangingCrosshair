a754b43021218350c32009a6ad7a7bcbcb1ac151
Muhammed Arslan <>
2021-07-12 12:58:58 +0300

    Delete: All Objects

--- Assets/Scripts/Character/MouseLook.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MouseLook : MonoBehaviour
-{
-    [Range(0.1f, 10f)]
-    [SerializeField] private float _sensitive;
-}
